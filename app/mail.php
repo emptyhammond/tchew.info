dbcd69030a81b67a0f1ab94a65fd4bec1c132fe8
emptyhammond <emptyhammond@gmail.com>
2012-09-17 10:51:09 +0100

    refersh - adding swift

---
+++ app/mail.php
@@ -0,0 +1,48 @@
+<?php
+require_once '../libs/swift-4.2.1/lib/swift_required.php';
+
+if(((boolean) $_POST['name']) and ((boolean) $_POST['email']) and ((boolean) $_POST['message']))
+{
+	$body = 'Name: '.$_POST['name']."\n\r";
+	$body .= 'Email: '.$_POST['email']."\n\r";
+	$body .= 'Message: '.$_POST['message'];
+
+	$username = 'AKIAJPIBV7XE7VVMSVMA';
+	$password = 'AibQvqC5JEYQ8lpiXs95/nTRJWUOWK8PZaI2s9HMzcAt';
+
+	$transport = Swift_SmtpTransport::newInstance('email-smtp.us-east-1.amazonaws.com', 465, 'ssl')
+					->setUsername($username)
+					->setPassword($password);
+
+	$mailer = Swift_Mailer::newInstance($transport);
+
+	$smessage = Swift_Message::newInstance();
+
+	$smessage = Swift_Message::newInstance()
+	  ->setSubject('Tchew.info - Contact Us')
+	  ->setFrom(array('matthew@matthewhammond.net' => 'Tchew Web Design'))
+	  ->setTo(array('contact@tchew.info', 'matthew@matthewhammond.net'))
+	  ->setBody($body);
+
+	try
+	{
+		$success = $mailer->send($smessage);
+	}
+	catch (Exception $e)
+	{
+		echo 0;
+	}
+
+	if ($success)
+	{
+		echo 1;
+	}
+	else
+	{
+		echo 0;
+	}
+}
+else
+{
+	echo 0;
+}
---
+++ js/script.js
@@ -0,0 +1,55 @@
+$('#navbar').scrollspy();
+
+$('#contact').submit(function(){
+
+	var name = $('#contact #name').val(),
+		email = $('#contact #email').val(),
+		message = $('#contact #message').val();
+
+
+	$('#contact #send').val('Sending...');
+
+	$.ajax({
+		url: '/app/mail.php',
+		type: 'POST',
+		data: {name: name,email: email,message: message},
+		success: function(data, textStatus, jqXHR){
+
+			$('#contact').find('.alert').hide();
+
+			if (data == 0)
+			{
+				var error =	'<div class="alert alert-block alert-error fade in"><button type="button" class="close" data-dismiss="alert">×</button><h4 class="alert-heading">Oh snap! You got an error!</h4><p>Sorry, your message hasn\'t been sent. Please try again - or contact us directly at <a href="mailto:contact@tchew.info">contact@tchew.info</a></p><p><a class="btn btn-danger" href="mailto:contact@tchew.info">contact@tchew.info</a></p></div>';
+
+				$('#contact').prepend(error);
+
+				$('#contact #send').val('Send');
+
+				setTimeout(function(){$('#contact').find('.alert').fadeOut();}, 20000);
+			}
+			else
+			{
+				var success =	'<div class="alert alert-block alert-success fade in"><button type="button" class="close" data-dismiss="alert">×</button><h4 class="alert-heading">Thanks for getting in touch</h4><p>Your message has been sent successfully, we\'ll be in touch soon.</p></div>';
+
+				$('#contact').prepend(success);
+
+				$('#contact').find('fieldset').slideUp();
+
+				$('#contact #send').val('Send');
+
+				setTimeout(function(){$('#contact').find('fieldset').slideDown();$('#contact').find('.alert').fadeOut();}, 10000);
+
+				name = $('#contact #name').val(''),
+				email = $('#contact #email').val(''),
+				message = $('#contact #message').val('');
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log(textStatus);
+		}
+	});
+
+	return false;
+});
+
+$(".alert").alert()
